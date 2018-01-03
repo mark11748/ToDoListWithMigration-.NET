99f78588dbbc7a7594ee06d5181fc338e8a0780c
James Osborn and mark w <james+mark@>
2018-01-03 15:28:27 -0800

    initial commit; migration db setup

---
+++ ToDoList/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ToDoList.Models
+{
+	public class Category
+	{
+		[Key]
+		public int CategoryId { get; set; }
+		public string Name { get; set; }
+		public virtual ICollection<Item> Items { get; set; }
+	}
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ToDoList.Models
+{
+	public class Item
+	{
+		[Key]
+		public int ItemId { get; set; }
+		public string Description { get; set; }
+		public int CategoryId { get; set; }
+		public virtual Category Category { get; set; }
+	}
+}
